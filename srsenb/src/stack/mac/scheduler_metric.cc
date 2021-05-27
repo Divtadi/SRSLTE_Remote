b188b1f2b3cda5332806829d86346ad5d9e1ad93
Divtadi <dtad7811@uni.sydney.edu.au>
2021-05-28 00:58:24 +1000

    added couts

--- srsenb/src/stack/mac/scheduler_metric.cc
+++ srsenb/src/stack/mac/scheduler_metric.cc
@@ -542,19 +542,19 @@ void ul_metric_rr::sched_ul_users_s2(std::map<uint16_t, sched_ue*>& ue_db, ul_sf
 bool ul_metric_rr::find_allocation(uint32_t L, ul_harq_proc::ul_alloc_t* alloc)
 {
   const prbmask_t* used_rb = &tti_alloc->get_ul_mask();
-  std::cout <<"Divya prb mask:" << used_rb->size()<< std::endl<<"/n";
-  bzero(alloc, sizeof(ul_harq_proc::ul_alloc_t));
-  std::cout <<"Divya alloc" << alloc->L << std::endl<<"/n";
-  std::cout <<"Divya size of UL lloc" << sizeof(ul_harq_proc::ul_alloc_t)<< std::endl<<"/n";
+  std::cout <<"Divya prb mask: " << used_rb->size()<< std::endl<<"/n";
+  bzero(alloc, sizeof(ul_harq_proc::ul_alloc_t));//not sure why L prints = 0
+  std::cout <<"Divya alloc:" << alloc->L << std::endl<<"/n";
+  std::cout <<"Divya size of UL alloc: " << sizeof(ul_harq_proc::ul_alloc_t)<< std::endl<<"/n";
   for (uint32_t n = 0; n < used_rb->size() && alloc->L < L; n++) {
-    std::cout <<"Divya L value" << L << std::endl<<"/n";
+    std::cout <<"Divya L RBs requested value: " << L << std::endl<<"/n";
     if (not used_rb->test(n) && alloc->L == 0) {
       alloc->RB_start = n;
-      std::cout <<"Divya Alloc RB_start value" << alloc->RB_start << std::endl<<"/n";
+      std::cout <<"Divya Alloc RB_start value: " << alloc->RB_start << std::endl<<"/n";
     }
     if (not used_rb->test(n)) {
       alloc->L++;
-      std::cout <<"Divya Alloc L++ value" << alloc->L << std::endl<<"/n";
+      std::cout <<"Divya Alloc L++ value: " << alloc->L << std::endl<<"/n";
     } else if (alloc->L > 0) {
       // avoid edges
       if (n < 3) {
@@ -567,6 +567,7 @@ bool ul_metric_rr::find_allocation(uint32_t L, ul_harq_proc::ul_alloc_t* alloc)
     }
   }
   if (alloc->L == 0) {
+    std::cout<<"Divya allocation  = 0, so returning false!" << std::endl<<"/n";
     return false;
   }
 
@@ -574,6 +575,7 @@ bool ul_metric_rr::find_allocation(uint32_t L, ul_harq_proc::ul_alloc_t* alloc)
   while (!srslte_dft_precoding_valid_prb(alloc->L)) {
     alloc->L--;
   }
+  std::cout <<"Divya Finally Allocated PRB value: " << alloc->L << std::endl<<"/n";
   return alloc->L == L;
 }
 /*bool ul_metric_rr::find_ul_allocation_slice(uint32_t L, ul_harq_proc::ul_alloc_t *alloc, sched_ue* user) { //user is defined here is it the same as the shed_ue *user?
